b8607c18eab7aa0d88cefe9b20e37d5bf4c3b39a
ghufr <ghufronfr@gmail.com>
2019-12-05 15:05:55 +0700

    New Version

---
+++ constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+  /* multiple choice:
+   * 0 (Sangat Puas),
+   * 1 (Puas),
+   * 2 (Tidak Puas),
+   * 3 (Sangat tidak puas)
+   */
+  multiple_choice: 0,
+  feedback:
+    "Sebelumnya saya mengucapkan terima kasih sekaligus meminta maaf karena tidak dapat memberikan feedback yang konstruktif pada survey ini. (https://github.com/ghufr/igracias-survey-automation)"
+};
--- main.js
+++ main.js
@@ -1,64 +1,101 @@
-require('dotenv').config();
-const { Builder, By } = require('selenium-webdriver');
+require("dotenv").config();
+const { Builder, By } = require("selenium-webdriver");
+const constants = require("./constants");
 
-const getRawLinks = (lectures) => {
-  const promises = lectures.map(async (lecture, i) => {
-    if (i > 0) {
-      return await lecture.findElement(By.tagName('a')).getAttribute('href').catch(() => {});
-    }
+const getRawLinks = links => {
+  const promises = links.map(async link => {
+    return await link.getAttribute("href").catch(() => {});
   });
   return Promise.all(promises);
-}
+};
 
-const getIds = (elements) => {
-  const promises = elements.map(async (element) => {
-    return await element.getLocation();
-  });
-  return Promise.all(promises);
-}
+const fillSurvey = async (links, driver) => {
+  for (link of links) {
+    await driver.get(link);
 
-(async function main() {
-  let driver = await new Builder().forBrowser('firefox').build();
-  try {
-    const baseUrl = 'https://igracias.telkomuniversity.ac.id';
-    let surveyPath = '/survey/index.php?pageid=2001&QID=3056';
-    const domain = '.igracias.telkomuniversity.ac.id';
-    await driver.get(baseUrl);
+    const hlm = await driver.findElements(By.className("hlm1"));
+    for (let i = 0; i < hlm.length; i++) {
+      const question = await driver.findElements(
+        By.xpath("//*[contains(@id, 'radio')]")
+      );
+      // Question each part
+      for (let index = 0; index < question.length; index++) {
+        await question[index]
+          .findElements(By.tagName("input"))
+          .then(async radio => {
+            await radio[constants.multiple_choice].click();
+          })
+          .catch(() => {});
+      }
+      await driver.findElement(By.xpath("//*[@value='simpan']")).click();
 
-    const PHPSESSID = process.env.PHPSESSID;
-    // const BIGipServerpool = '3002924042.20480.0000';
-
-    await driver.manage().addCookie({ name: 'BIGipServerpool_iGracias', value: BIGipServerpool, domain, httpOnly: true, secure: true })
-    await driver.manage().addCookie({ name: 'PHPSESSID', value: PHPSESSID, domain })
-    await driver.get(baseUrl + surveyPath)
-    const lectures = await driver.findElements(By.xpath("//*[@id='form1']/table/tbody/child::tr"));
-
-    const rawLink = await getRawLinks(lectures);
-    for(link of rawLink) {
-      if (link) {
-        await driver.get(link)
-        for(let i = 0; i < 2; i++) {
-          const question = await driver.findElements(By.xpath("//*[contains(@id, 'radio')]"));
-          for (let index = 0; index < 10; index++) {
-            await question[index].findElements(By.tagName('input')).then(async (radio) => {
-              await radio[0].click();
-            }).catch(() => {});
-          }
-          await driver.findElement(By.xpath("//*[@value='simpan']")).click();
-        }
-        const textBox = await driver.findElement(By.tagName('textarea'));
-        await textBox.clear();
-        await textBox.sendKeys('Saran dan survey ini sepenuhnya dibuat secara otomatis oleh software, saya sedang melakukan uji coba software pengisi survey igracias secara otomatis :)')
-        await driver.findElement(By.xpath("//*[@value='simpan']")).click();
+      // Search for textbox
+      const textBoxs = await driver.findElements(By.tagName("textarea"));
+      for (textbox of textBoxs) {
+        await textbox.clear();
+        await textbox.sendKeys(constants.feedback);
       }
     }
-    await driver.get(baseUrl + surveyPath);
-    const submits = await driver.findElement(By.xpath("//*[contains(@href, 'APPLY')]"));
-    const ids = await getIds(submits);
-    for (id of ids) {
-      await driver.findElement(id).click();
+    await driver.findElement(By.xpath("//*[@value='simpan']")).click();
+  }
+  await driver.findElement(By.xpath("//input[@class='floatL4']")).click();
+};
+
+(async function main() {
+  let driver = await new Builder().forBrowser("firefox").build();
+  const pageid = 2001;
+
+  try {
+    const baseUrl = "https://igracias.telkomuniversity.ac.id";
+    let surveyPath = `survey/index.php?pageid=${pageid}`;
+    const surveyUrl = `${baseUrl}/${surveyPath}`;
+    // const domain = ".igracias.telkomuniversity.ac.id";
+    await driver.get(baseUrl);
+    await driver.sleep(2000);
+
+    // Password Login
+    const styl = "arguments[0].style";
+    const js = `${styl}.height='400px'; ${styl}.marginTop='-100px';`;
+
+    const login_form = await driver.findElement(By.id("ac_subitem_login"));
+    await driver.executeScript(js, login_form);
+
+    // await login_menu.findElement(By.id("login_button_general")).click();
+
+    const form = await driver.findElement(By.name("loginform"));
+    await form
+      .findElement(By.id("textUsername"))
+      .sendKeys(process.env.IGRACIAS_USERNAME);
+    await form
+      .findElement(By.id("textPassword"))
+      .sendKeys(process.env.IGRACIAS_PASSWORD);
+    // return;
+    await form.findElement(By.id("submit")).click();
+    await driver.sleep(1000);
+    await driver.get(surveyUrl);
+
+    const surveys = await driver.findElements(By.xpath("//*[@id='form1']//a"));
+    const survey_url = await getRawLinks(surveys);
+
+    for (survey of survey_url) {
+      await driver.get(survey);
+
+      const isSurvey = await driver.findElements(By.className("hlm2"));
+
+      if (isSurvey.length > 0) {
+        // Fill Survey
+        await fillSurvey([survey], driver);
+      } else {
+        // There still a menu
+        const lectures = await driver.findElements(
+          By.xpath("//*[@id='form1']//a")
+        );
+        const lectures_url = await getRawLinks(lectures);
+        await fillSurvey(lectures_url, driver);
+        // await driver.get(survey);
+      }
     }
-  } catch(e) {
-    console.log(e)
+  } catch (e) {
+    console.log(e);
   }
 })();
